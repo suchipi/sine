a4243c329cb680750eb5aee0bdf29593d49a4924
Suchipi Izumi <me@suchipi.com>
2019-10-03 19:37:54 -0600

    wip

--- Sine/GameScene.swift
+++ Sine/GameScene.swift
@@ -16,6 +16,7 @@ class GameScene: SKScene {
     private let engine = AVAudioEngine.init()
     private let player = AVTonePlayerUnit.init()
     
+    // MARK: - Lifecycle methods
     override func didMove(to view: SKView) {
         self.frequencyLabel = self.childNode(withName: "frequencyLabel") as? SKLabelNode
         self.noteNameLabel = frequencyLabel?.childNode(withName: "noteNameLabel") as? SKLabelNode
@@ -24,7 +25,7 @@ class GameScene: SKScene {
         noteNameLabel?.text = ""
         
         player.amplitude = 1.0
-        player.frequency = 880
+        player.frequency = 440
         engine.attach(player)
         engine.connect(player, to: engine.mainMixerNode, format: AVAudioFormat.init(standardFormatWithSampleRate: player.sampleRate, channels: 1))
         engine.prepare()
@@ -39,44 +40,7 @@ class GameScene: SKScene {
         engine.stop()
     }
     
-    func updateRate(fromPoint point: CGPoint) {
-        // Bottom of the screen should be 0, middle should be 1, top should be 2
-        let rate = (((0.5 * self.size.height) + point.y) / self.size.height) * 2 // 0.0 to 2.0, where 1.0 is normal speed
-        let frequency = Double(880 * rate)
-        player.frequency = frequency
-        frequencyLabel?.text = NSString(format: "%.0fHz", frequency) as String
-        
-        if let noteName = FrequencyTable.getNoteName(frequency: frequency) {
-            noteNameLabel?.text = noteName as String
-        }
-    }
-    
-    func updateVolume(fromPoint point: CGPoint) {
-        // Left side of the screen should be 0, right side of the screen should be 1
-        let volume = ((0.5 * self.size.width) + point.x) / self.size.width
-        player.amplitude = Double(volume)
-    }
-    
-    func touchDown(atPoint point: CGPoint) {
-        updateRate(fromPoint: point)
-        updateVolume(fromPoint: point)
-        
-        player.preparePlaying()
-        player.play()
-    }
-    
-    func touchMoved(toPoint point: CGPoint) {
-        updateRate(fromPoint: point)
-        updateVolume(fromPoint: point)
-    }
-    
-    func touchUp(atPoint pos: CGPoint) {
-        player.pause()
-        player.reset()
-        frequencyLabel?.text = ""
-        noteNameLabel?.text = ""
-    }
-    
+    // MARK: - Touch lifecycle methods
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
@@ -91,5 +55,44 @@ class GameScene: SKScene {
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
         for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    // MARK: - Helpers
+    private func updateRate(fromPoint point: CGPoint) {
+        // Bottom of the screen should be 0, middle should be 1, top should be 2
+        let rate = (((0.5 * self.size.height) + point.y) / self.size.height) * 2 // 0.0 to 2.0, where 1.0 is normal speed
+        let frequency = Double(440 * rate)
+        player.frequency = frequency
+        frequencyLabel?.text = NSString(format: "%.0fHz", frequency) as String
+        
+        if let noteName = FrequencyTable.getNoteName(frequency: frequency) {
+            noteNameLabel?.text = noteName as String
+        }
+    }
+    
+    private func updateVolume(fromPoint point: CGPoint) {
+        // Left side of the screen should be 0, right side of the screen should be 1
+        let volume = ((0.5 * self.size.width) + point.x) / self.size.width
+        player.amplitude = Double(volume)
+    }
+    
+    func touchDown(atPoint point: CGPoint) {
+        updateRate(fromPoint: point)
+        updateVolume(fromPoint: point)
+        
+        player.preparePlaying()
+        player.play()
+    }
+    
+    private func touchMoved(toPoint point: CGPoint) {
+        updateRate(fromPoint: point)
+        updateVolume(fromPoint: point)
+    }
+    
+    private func touchUp(atPoint pos: CGPoint) {
+        player.pause()
+        player.reset()
+        frequencyLabel?.text = ""
+        noteNameLabel?.text = ""
     }
 }
